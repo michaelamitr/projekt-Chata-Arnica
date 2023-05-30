3820e744750c05d62f5d4bed90d1b32118474b3e
Michaela Trčková <misa.trckova@volny.cz>
2023-05-30 19:29:57 +0200

    desktop, dark mode, done

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ const toggleSwitch = document.querySelector(
 const switchTheme = (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
   }
 };
+
+const currentTheme = localStorage.getItem('theme')
+  ? localStorage.getItem('theme')
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
 
 toggleSwitch.addEventListener('change', switchTheme, false);
